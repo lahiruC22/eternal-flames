a16f5306164a8829a9b76140e0638a70af3cf4c2
lahiruC22 <as2021356@sci.sjp.ac.lk>
2026-02-06 22:43:48 +0530

    chore(github): fix workflow fails including deployment

---
+++ lib/placeholder-images.ts
@@ -0,0 +1,27 @@
+export const PlaceHolderImages = [
+  {
+    id: 'meeting',
+    imageUrl: "https://images.unsplash.com/photo-1513279922550-250c2129b13a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw4fHxjb3VwbGUlMjBzdW5zZXR8ZW58MHx8fHwxNzY5OTk2NDkyfDA&ixlib=rb-4.1.0&q=80&w=1080",
+    alt: 'Our First Meeting',
+  },
+  {
+    id: 'date',
+    imageUrl: "https://images.unsplash.com/photo-1573822028151-731623cb0722?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw1fHxjb3p5JTIwY2FmZXxlbnwwfHx8fDE3NzAwNDMzNjR8MA&ixlib=rb-4.1.0&q=80&w=1080",
+    alt: 'First Date',
+  },
+  {
+    id: 'adventure',
+    imageUrl: "https://images.unsplash.com/photo-1592254765718-02cdd14502fc?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwzfHxob2xkaW5nJTIwaGFuZHN8ZW58MHx8fHwxNzcwMTAyMzEyfDA&ixlib=rb-4.1.0&q=80&w=1080",
+    alt: 'Our Adventure',
+  },
+  {
+    id: 'special',
+    imageUrl: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw0fHxmb3Jlc3QlMjBwYXRofGVufDB8fHx8MTc3MDAxNzM5OHww&ixlib=rb-4.1.0&q=80&w=1080",
+    alt: 'Special Moment',
+  },
+  {
+    id: 'valentine',
+    imageUrl: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw0fHxmb3Jlc3QlMjBwYXRofGVufDB8fHx8MTc3MDAxNzM5OHww&ixlib=rb-4.1.0&q=80&w=1080",
+    alt: "Valentine's Day 2026",
+  },
+] as const;
